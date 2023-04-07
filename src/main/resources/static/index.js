8d2e5231583ceca835148b4cd00b1dae48bc27a6
JHying <ritahying@gmail.com>
2023-04-07 14:37:40 +0800

    撥出前先做連線確認 (failed to execute 'send' on 'websocket': still in connecting state)

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -3,8 +3,8 @@ let remoteVideo = document.getElementById('remoteVideo');
 let websocket = null;
 let peer = null;
 
-FormInit();
 WebSocketInit();//初始化 websocket
+FormInit();
 ButtonFunInit();
 
 /* WebSocket */
@@ -42,17 +42,18 @@ function WebSocketInit() {
             //掛斷電話
             case 'hangup':
                 if(obj.sender === 'SYSTEM'){
-                    tempAlert("對象不存在", 5000);
+                    tempAlert("對象不存在", 7000);
                 } else {
-                    tempAlert("通話已結束", 5000);
+                    tempAlert("通話已結束", 7000);
                 }
                 document.getElementById('hangup').click();
                 return;
 
             //發起通話請求
             case 'call_start':
-                if (confirm(obj.sender + " 來電，是否接聽？") == true) {
-                    tempAlert("遠距中心來電<br>通話即將開始", 3000);
+                //只有病人會需要確認，但礙於到時候可能無法點選，所以用alert自動接聽
+                //if (confirm(obj.sender + " 來電，是否接聽？") == true) {
+                    tempAlert("遠距中心來電<br>通話即將開始", 6000);
                     //將連接資訊回覆給 sender
                     document.getElementById('receiver').value = obj.sender;
                     document.getElementById('receiver').style.visibility = 'hidden';//接聽後隱藏表單
@@ -64,15 +65,15 @@ function WebSocketInit() {
                         receiver: obj.sender,
                         sender: username,
                     }));
-                } else {
-                    //拒絕通話掛掉電話
-                    websocket.send(JSON.stringify({
-                        type: "hangup",
-                        receiver: obj.sender,
-                        sender: username,
-                    }));
-                }
-                return;
+//                } else {
+//                    //拒絕通話掛掉電話
+//                    websocket.send(JSON.stringify({
+//                        type: "hangup",
+//                        receiver: obj.sender,
+//                        sender: username,
+//                    }));
+//                }
+//                return;
 
             //接收到通話請求回覆，傳送 rtc offer 給對方
             case 'call_back':
@@ -123,8 +124,8 @@ function WebSocketInit() {
 function WebRTCInit() {
     const ice = {
       "iceServers": [
-        {"url": "stun:stun.l.google.com:19302"}, //for跨網段
-        // {"url": "turn:turnserver.com", "username": "user", "credential": "pass"}
+        {"url": "stun:stun.l.google.com:19302"},
+        // {"url": "turn:turnserver.com", "username": "user", "credential": "pass"} //範例
       ]
     };
     peer = new RTCPeerConnection(ice);
@@ -158,12 +159,12 @@ function ButtonFunInit() {
         } else {
             document.getElementById('receiver').style.visibility = 'hidden';//撥打後隱藏表單
             document.getElementById('call').setAttribute("disabled", "disabled");//撥打後disable通話鍵
-            tempAlert("等待接聽...", 3000);//顯示撥打訊息
+            tempAlert("等待接聽...", 7000);//顯示撥打訊息
         }
         if (peer == null) {
             WebRTCInit();
         }
-        websocket.send(JSON.stringify({
+        sendMessage(JSON.stringify({
             type: "call_start",
             sender: username,
             receiver: receiver,
@@ -205,8 +206,8 @@ function ButtonFunInit() {
 
 function FormInit() {
     if(username !== 'center') {
-        document.getElementById("receiver").style.display = 'none';//統一從center發起通話
-        document.getElementById("buttons").style.display = 'none';//統一從center控制通話
+        document.getElementById("receiver").style.display = 'none';//統一從醫院端發起通話
+        document.getElementById("buttons").style.display = 'none';//統一從醫院端控制通話
     } else {
         if(target !== null) {
             //如果有指定對象，則綁定對象(不給修改)
@@ -222,14 +223,37 @@ function tempAlert(msg, duration) {
  var el = document.createElement("div");
  el.setAttribute("style","position:absolute;color: white;background-color:black;font-size: 2em");
  el.innerHTML = msg;
+ el.id = 'msg';
  setTimeout(function(){
   el.parentNode.removeChild(el);
- },duration);
+ }, duration);
  document.body.appendChild(el);
 }
 
-function delay(n){
+function delay(n) {
     return new Promise((r) => {
         setTimeout(r, n * 1000);
     });
+}
+
+function sendMessage(msg) {
+    // Wait until the state of the socket is not ready and send the message when it is...
+    WaitForSocketConnection(websocket, function() {
+        websocket.send(msg);
+    });
+}
+
+// Make the function wait until the connection is made...
+function waitForSocketConnection(socket, callback) {
+    setTimeout(
+        function () {
+            if (socket.readyState === 1) {
+                if (callback != null){
+                    callback();
+                }
+            } else {
+                tempAlert("等待接聽...", 1000);
+                waitForSocketConnection(socket, callback);
+            }
+        }, 5); // wait 5 milisecond for the connection...
 }
